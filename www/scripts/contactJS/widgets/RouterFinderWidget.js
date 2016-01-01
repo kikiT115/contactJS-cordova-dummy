54441958d52de38610b74ada39aaae85620b108f
Hendrik Geßner <HENGE01@gmail.com>
2016-01-01 21:16:26 +0100

    Added dummy RouterFinderWidget to demonstrate WidgetCreator usage.

---
+++ www/scripts/contactJS/widgets/RouterFinderWidget.js
@@ -0,0 +1,30 @@
+define(['contactJS', './WidgetCreator'], function (contactJS, WidgetCreator) {
+    return (function() {
+        var RouterFinderWidget = WidgetCreator.extend("RouterFinderWidget", {
+            description: {
+                out: [
+                    {
+                        "name": "CI_ROUTER_URL",
+                        "type": "STRING",
+                        "parameterList": [["CP_UNIT", "STRING", "URL"]]
+                    }
+                ],
+                const: [
+                    {
+                        "name": "",
+                        "type": ""
+                    }
+                ],
+                updateInterval: 5000
+            },
+            simpleQueryGenerator: function(callback) {
+                var routerUrl = "http://192.168.2.1/login.html";
+                callback({0: routerUrl});
+
+                console.log("Hendrik: response sent");
+            }
+        });
+
+        return RouterFinderWidget;
+    })();
+});
