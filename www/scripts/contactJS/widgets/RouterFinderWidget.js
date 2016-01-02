2fc466706ce5b767ee786d977f94c7244fb846ac
Hendrik Geßner <HENGE01@gmail.com>
2016-01-02 12:17:55 +0100

    Further reduced RouterFinderWidget code.

--- www/scripts/contactJS/widgets/RouterFinderWidget.js
+++ www/scripts/contactJS/widgets/RouterFinderWidget.js
@@ -1,30 +1,20 @@
 define(['contactJS', './WidgetCreator'], function (contactJS, WidgetCreator) {
-    return (function() {
-        var RouterFinderWidget = WidgetCreator.extend("RouterFinderWidget", {
-            description: {
-                out: [
-                    {
-                        "name": "CI_ROUTER_URL",
-                        "type": "STRING",
-                        "parameterList": [["CP_UNIT", "STRING", "URL"]]
-                    }
-                ],
-                const: [
-                    {
-                        "name": "",
-                        "type": ""
-                    }
-                ],
-                updateInterval: 5000
-            },
-            simpleQueryGenerator: function(callback) {
-                var routerUrl = "http://192.168.2.1/login.html";
-                callback({0: routerUrl});
+    return WidgetCreator.extend("RouterFinderWidget", {
+        description: {
+            out: [
+                {
+                    "name": "CI_ROUTER_URL",
+                    "type": "STRING",
+                    "parameterList": [["CP_UNIT", "STRING", "URL"]]
+                }
+            ],
+            updateInterval: 5000
+        },
+        simpleQueryGenerator: function(callback) {
+            var routerUrl = "http://192.168.2.1/login.html";
+            callback({0: routerUrl});
 
-                console.log("Hendrik: response sent");
-            }
-        });
-
-        return RouterFinderWidget;
-    })();
+            console.log("Hendrik: response sent");
+        }
+    });
 });
