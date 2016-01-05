1803e71da766830262af4b75099f1f71f4a03126
Hendrik Geßner <HENGE01@gmail.com>
2016-01-05 11:19:31 +0100

    Added DeviceOsWidget.

---
+++ www/scripts/contactJS/widgets/DeviceOsWidget.js
@@ -0,0 +1,21 @@
+define(['contactJS', 'jquery', './WidgetCreator'], function(contactJS, $, WidgetCreator) {
+    return WidgetCreator.extend("DeviceOsWidget", {
+        description: {
+            out: [
+                {
+                    "name": "CI_DEVICE_OS",
+                    "type": "STRING",
+                    "parameterList": [["CP_UNIT", "STRING", "DEVICE_OS"]]
+                },
+                {
+                    "name": "CI_DEVICE_VERSION",
+                    "type": "STRING",
+                    "parameterList": [["CP_UNIT", "STRING", "DEVICE_VERSION"]]
+                }
+            ],
+            updateInterval: 10000
+        },
+        simpleQueryGenerator: function(callback) {
+        }
+    });
+});
