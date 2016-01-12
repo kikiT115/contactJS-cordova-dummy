73c2be7b1b19856feee1548809328a540f923c95
Hendrik Geßner <HENGE01@gmail.com>
2016-01-12 11:34:26 +0100

    Added InterpreterCreator. Missing documentation.

---
+++ www/scripts/contactJS/interpreters/InterpreterCreator.js
@@ -0,0 +1,63 @@
+define(['contactJS'], function(contactJS) {
+
+    var getInputData = function(inContextInformation) {
+        var result = [];
+
+        var items = this.getInputContextInformation().getItem();
+        for (var itemIndex in items) {
+            var value = items[itemIndex];
+            result.push(inContextInformation.getValueForContextInformationOfKind(value));
+        }
+
+        return result;
+    };
+
+    var setOutputData = function(values, outContextInformation) {
+        var rawValues = outContextInformation.getItems();
+        for (var rawValueIndex in rawValues) {
+            if (values.hasOwnProperty(rawValueIndex)) {
+                var value = values[rawValueIndex];
+                var rawValue = rawValues[rawValueIndex];
+                rawValue.setValue(value);
+            }
+        }
+        return rawValues;
+    };
+
+    var createSimpleQueryGenerator = function(simpleQueryGenerator) {
+        return function(inContextInformation, outContextInformation, callback) {
+            var sendResponse = function(simplifiedResponse) {
+                setOutputData(simplifiedResponse, outContextInformation);
+                callback();
+            };
+
+            var values = getInputData(inContextInformation);
+            simpleQueryGenerator.call(this, values, sendResponse);
+        };
+    };
+
+    var InterpreterCreator = {
+        extend: function(name, properties) {
+            var createdInterpreter = function(discoverer) {
+                contactJS.Interpreter.call(this, discoverer);
+                this._name = name;
+                return this;
+            };
+
+            // Use known defaults
+            createdInterpreter.prototype = Object.create(contactJS.Interpreter.prototype);
+            createdInterpreter.prototype.constructor = createdInterpreter;
+
+            // Choose queryGenerator. Use simplified version if available
+            var interpretData = properties.simpleInterpretData ? createSimpleQueryGenerator(properties.simpleInterpretData) : properties._interpretData;
+
+            // Copy properties into Widget
+            createdInterpreter.description = properties.description;
+            createdInterpreter.prototype._interpretData = interpretData;
+
+            return createdInterpreter;
+        }
+    };
+
+    return InterpreterCreator;
+});
